b6c3e0a9d64607904f63b3f4c06979c9ba45d442
liyebang <lyb2017work@163.com>
2019-07-13 15:56:39 +0800

    基本结构

---
+++ pages/index/index.js
@@ -0,0 +1,70 @@
+// pages/search/search.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      "http://img2.imgtn.bdimg.com/it/u=1353877914,2272011194&fm=26&gp=0.jpg",
+      "http://img1.imgtn.bdimg.com/it/u=1458888286,673355229&fm=26&gp=0.jpg",
+      "http://img2.imgtn.bdimg.com/it/u=2133019295,1525250475&fm=26&gp=0.jpg"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
