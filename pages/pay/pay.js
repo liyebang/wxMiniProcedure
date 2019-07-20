bfb32e908f2124aff130a9deb768ada03b7bff36
liyebang <lyb2017work@163.com>
2019-07-20 20:07:16 +0800

    创建支付订单

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -1,4 +1,8 @@
-// pages/pay/pay.js
+// 小程序使用 async await 语法的时候，会报错，解决办法：引入 regeneratorRuntime 包
+const regeneratorRuntime = require("../../lib/runtime/runtime.js");
+
+const { request } = require("../../utils/request.js");
+
 Page({
 
   /**
@@ -85,7 +89,7 @@ Page({
   },
 
   // 点击结算
-  handlePay(){
+  async handlePay(){
     
     // 1. 先检验本地有没有 token，没有 token 就跳转到登录授权
     const token = wx.getStorageSync("token");
@@ -97,5 +101,63 @@ Page({
       // 没有授权就退出函数
       return;
     }
+
+    // 支付流程 - 要按顺序执行 - 如何从上往下执行 - 可以封装成方法，内部返回 Promise 对象
+
+    const {
+      cartlist,
+      address,
+      total
+    } = this.data;
+
+    // 创建用于创建订单的商品对象
+    const goods =
+      // 遍历购物车集合的 keys
+      Object.keys(cartlist)
+        // filter 过滤选中状态的商品
+        .filter(id => cartlist[id].selected)
+        // map 返回新对象，用于创建订单
+        .map(id => {
+          return {
+            goods_id: cartlist[id].goods_id,
+            goods_price: +cartlist[id].goods_price,
+            goods_number: cartlist[id].count
+          }
+        });
+
+    // 创建订单
+    const params = {
+      "order_price": total,
+      "consignee_addr": address.addressInfo,
+      "goods": goods
+    }
+
+    // 1. 创建订单，获取订单编号
+    const {
+      order_number
+    } = (await this.getOrderNumber(params));
+    console.log(order_number, '1. 创建订单，获取订单编号');
+    // 2. 根据订单编号，准备预支付
+    console.log("2. 根据订单编号，准备预支付");
+    // 3. 根据预支付数据发起微信支付
+    // 4. 微信支付成功后，查询订单，更新订单状态
+  },
+
+  // 1.创建订单，获取订单编号
+  getOrderNumber(params) {
+    // 获取 token 
+    const token = wx.getStorageSync('token');
+    // 调用接口，需要带上 token
+    return request({
+      url: 'my/orders/create',
+      method: "POST",
+      // 请求头带上 token
+      header: {
+        "Authorization": token
+      },
+      data: {
+        ...params
+      }
+    })
   }
 })
